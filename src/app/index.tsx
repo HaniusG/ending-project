59d501c293555de5004ab7bf2af8b45653f3e500
Grigor Ohanyan <ohanyang183@gmail.com>
2023-11-08 19:16:35 +0400

    commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,11 @@ import "./styles/index.scss";
 import React, { Suspense, lazy, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 const MainPage = lazy(() => import('pages/MainPage'))
+const StudentPage = lazy(() => import('pages/StudentPage'))
+const TeacherPage = lazy(() => import('pages/TeacherPage'))
+const LoginPage = lazy(() => import('pages/LoginPage'))
+
+
 
 
 
@@ -12,7 +17,12 @@ const App: React.FC = () => {
     <BrowserRouter>
         <Suspense fallback={"Loading"}>
         <Routes>
-            <Route path='/main' element={<MainPage/>} />
+            <Route path='/' element={<MainPage/>} />
+            <Route path='/teacher' element={<TeacherPage/>} />
+            <Route path='/togin' element={<LoginPage/>} />
+            <Route path='/ttudent' element={<StudentPage/>} />
+
+
           </Routes>
         </Suspense>
     </BrowserRouter>
