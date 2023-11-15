05a816f057427d3e48e475657af05d78433ca7f4
Grigor Ohanyan <ohanyang183@gmail.com>
2023-11-15 17:16:08 +0400

    commit

--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -7,47 +7,48 @@ import SideNavBar from "widgets/SideNavBar/SideNavBar";
 
 const UserPage: React.FC<UserPageProps> = ({ user, handleSignOut }) => {
   return (
-    <div>
-       
-        <div className={styles.user}>
-      <div className={styles.userProfile}>
-        <div className={styles.userAvatar}>
-         
-          
-          <div className={styles.indicateCircle}></div>
-        </div>
-        <div className={styles.userInfo}>
-          <h2>{user?.displayName}</h2>
-          <p>Student</p>
-        </div>
-
-        <div className={styles.personalInfo}>
-          <h1>Personal Info</h1>
-
-          <div className={styles.infoContainer}>
-            <h2 className={styles.info}>{user?.displayName}</h2>
-            <span className={styles.changeBtn}>John Doe</span>
+      <div className={styles.user}>
+        <div className={styles.userProfile}>
+          <div className={styles.userAvatar}>
+            {
+              user?.photoURL ?  <img src={user.photoURL} alt="" />: <div>No image</div>
+            }
+            
+            <div className={styles.indicateCircle}></div>
+          </div>
+          <div className={styles.userInfo}>
+            <h2>{user?.displayName}</h2>
+            <p>Student</p>
           </div>
 
-          <div className={styles.infoContainer}>
-            <h2 className={styles.info}>Job title</h2>
-            <span className={styles.changeBtn}>Teacher</span>
-          </div>
-          
-          <div className={styles.infoContainer}>
-            <h2 className={styles.info}>Mail</h2>
-            <span className={styles.changeBtn}>{user?.email}</span>
-          </div>
+          <div className={styles.personalInfo}>
+            <h1>Personal Info</h1>
 
-          <div className={styles.infoContainer}>
-            <h2 className={styles.info}>Password</h2>
-            <span className={styles.changeBtn}>*********</span>
+            <div className={styles.infoContainer}>
+              <h2 className={styles.info}>{user?.displayName}</h2>
+              <span className={styles.changeBtn}>John Doe</span>
+            </div>
+
+            <div className={styles.infoContainer}>
+              <h2 className={styles.info}>Job title</h2>
+              <span className={styles.changeBtn}>Teacher</span>
+            </div>
+
+            <div className={styles.infoContainer}>
+              <h2 className={styles.info}>Mail</h2>
+              <span className={styles.changeBtn}>{user?.email}</span>
+            </div>
+
+            <div className={styles.infoContainer}>
+              <h2 className={styles.info}>Password</h2>
+              <span className={styles.changeBtn}>*********</span>
+            </div>
+            <button onClick={handleSignOut}>
+            </button>
           </div>
-          <button onClick={handleSignOut}><Link to="/">Sign out</Link></button>
         </div>
       </div>
-    </div>
-        </div>  
+
   );
 };
 
